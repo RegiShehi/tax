53c1763b0c8691b0fc1870e35596c05eff669be2
RegiShehi <shehi.regi@gmail.com>
2021-11-23 18:56:58 +0100

    Add nginx configuration

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'http://0.0.0.0:1337/api',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '38c16c3a2a5a71b0c5bc65058e573ed2'),
     },
-    url: env('PUBLIC_ADMIN_URL', '/dashboard'),
+    url: 'http://0.0.0.0:1337/dashboard',
   },
 });
