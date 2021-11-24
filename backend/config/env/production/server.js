aaa89c50bd16e6c4c4cccc58f995bc86acae42d4
RegiShehi <shehi.regi@gmail.com>
2021-11-24 21:22:31 +0100

    Update prod env

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '3.69.49.246'),
   port: env.int('PORT', 1337),
-  url: 'http://0.0.0.0:1337/api',
+  url: 'http://3.69.49.246:1337/api',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '38c16c3a2a5a71b0c5bc65058e573ed2'),
     },
-    url: 'http://0.0.0.0:1337/dashboard',
+    url: 'http://3.69.49.246:1337/dashboard',
   },
 });
